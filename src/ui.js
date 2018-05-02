586b15e4b745b3822da078f66439c9a830e45fb8
ernscht <ernscht@gmail.com>
2018-05-02 17:36:24 +0200

    give more control in entry points - better dependency tree

--- src/ui.js
+++ src/ui.js
@@ -3,38 +3,21 @@
 import * as T from 'terrific';
 import $ from 'jquery';
 
+import './shared/base/security/js/security';
+import './shared/base/reset/css/reset.scss';
+import './shared/base/document/css/document.scss';
+
+import './patterns/atoms/box';
+import './patterns/atoms/button';
+import './patterns/atoms/cta';
+import './patterns/atoms/icon';
+import './patterns/atoms/image';
+import './patterns/atoms/loader';
+import './patterns/molecules/example';
+
 if (module.hot) {
 	module.hot.accept();
 }
-
-function requireAll(requireContext) {
-	return requireContext.keys().map(requireContext);
-}
-
-/* eslint-disable no-useless-escape */
-
-// js files
-requireAll(require.context('./shared', true,
-	/(?:base)[\/\\]([^\/\\]+)[\/\\]js[\/\\]\1\.js$/
-));
-requireAll(require.context('./patterns', true,
-	/(?:atoms|molecules|organisms|helpers)[\/\\]([^\/\\]+)[\/\\]js[\/\\]\1\.js$/
-));
-requireAll(require.context('./patterns', true,
-	/(?:atoms|molecules|organisms|helpers)[\/\\]([^\/\\]+)[\/\\]js[\/\\]decorator[\/\\][^\/\\]+\.js$/
-));
-
-// css files
-requireAll(require.context('./shared', true,
-	/(?:base)[\/\\]([^\/\\]+)[\/\\]css[\/\\]\1\.s?css$/
-));
-requireAll(require.context('./patterns', true,
-	/(?:atoms|molecules|organisms|helpers)[\/\\]([^\/\\]+)[\/\\]css[\/\\]\1\.s?css$/
-));
-requireAll(require.context('./patterns', true,
-	/(?:atoms|molecules|organisms|helpers)[\/\\]([^\/\\]+)[\/\\]css[\/\\]modifier[\/\\][^\/\\]+\.s?css$/
-));
-/* eslint-enable no-useless-escape */
 
 // custom code
 console.log('I\'m from the entry point'); // eslint-disable-line

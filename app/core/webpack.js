ee585764a01c26981371c2274ca6a6fd19e54a0d
ernscht <ernscht@gmail.com>
2018-05-14 14:49:10 +0200

    bump deps

--- app/core/webpack.js
+++ app/core/webpack.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
